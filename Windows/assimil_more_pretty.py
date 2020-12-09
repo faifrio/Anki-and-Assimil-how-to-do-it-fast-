061b6a0463a4c9a02d6f6bf47068056eee56b940
faifrio <75743205+faifrio@users.noreply.github.com>
2020-12-09 16:39:48 +0100

    Windows

---
+++ Windows/assimil_more_pretty.py
@@ -0,0 +1,20 @@
+import sys
+from os import listdir
+from shutil import copyfile
+
+path_in = str(sys.argv[1])
+path_out = str(sys.argv[2])
+
+lesson_folders = listdir(path_in)
+lesson_folders = sorted(lesson_folders)
+for folder in lesson_folders:
+    if folder[0] == "L":
+        path = path_in + folder + "\\"
+        files_folder = listdir(path)
+        files_folder = sorted(files_folder)
+
+        row_list = []
+        for audio in files_folder:
+            if audio[0] == "S" and "00" not in audio:
+                copyfile(path + audio, path_out + folder[:4] + "-" + audio)
+                print(path + audio, path_out + folder[:4] + "-" + audio)
